50b1fed40964fd3d6578f1c8c13daa18899535ae
VitthalS <vitthal.shinde95@gmail.com>
2021-01-21 20:48:04 +0530

    Removed extra message

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'From Javascript'
+messageOne.textContent = ''
 messageTwo.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
